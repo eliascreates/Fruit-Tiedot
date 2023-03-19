3eca4b43d4d9490a5e71844da09fc7928c9d7a74
Elias Kekana <thapeloelias.kekana@gmail.com>
2023-03-19 17:52:42 +0200

    Initial Commit

---
+++ android/app/src/main/kotlin/com/example/fruit_tiedot/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.fruit_tiedot
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/constants.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+const Color primaryColor = Color(0xFFA3F3EB);
+const Color secoundaryColor = Color(0xFFFBD341);  
+const Color backgroundColor = Color(0xFFF1FFAB);
+
+const Color accentColor = Color(0xFFFB9A40);
+const Color errorColor = Color(0xFFFF2442);
+
+const double defaultPadding = 20.0;
